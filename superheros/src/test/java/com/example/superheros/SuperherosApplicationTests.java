5ad12f71cc2e9584daaa1923337c972199167b7a
ZhongzhouYang <zhongzhou.yang@fs-students.de>
2023-07-28 09:45:48 +0100

    springboot

---
+++ superheros/src/test/java/com/example/superheros/SuperherosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.superheros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperherosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
