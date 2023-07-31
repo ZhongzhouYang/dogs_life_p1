2190cede589d278b27519c11b6d617849dc6cefe
ZhongzhouYang <zhongzhou.yang@fs-students.de>
2023-07-31 16:10:56 +0100

    react

---
+++ superheroes-ui/src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import AllHeroes from './components/AllHeroes';
+import HeroDetail from './components/HeroDetail';
+
+function App() {
+  return (
+    <AllHeroes/>
+  );
+}
+
+export default App;
